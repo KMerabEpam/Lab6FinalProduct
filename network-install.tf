0bbfa4c1f35a2ee8faddfdf170efd9e196dcc503
KMerabEpam <95089207+KMerabEpam@users.noreply.github.com>
2021-12-01 12:07:15 +0400

    new update

--- network-install.tf
+++ network-install.tf
@@ -1,60 +1,35 @@
-#Create VPC in us-east-1
-resource "aws_vpc" "vpc_master" {
-  provider             = aws.region-master
-  cidr_block           = "10.0.0.0/24"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+module "vpc-1" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.0.0/24"]
+
+  enable_nat_gateway = true
+
+
   tags = {
-    Name = "master-vpc-zona_a"
+    Terraform = "true"
+    Environment = "dev"
   }
-
 }
 
-#Create VPC in us-west-2
-resource "aws_vpc" "vpc_master_oregon" {
-  provider             = aws.region-worker
-  cidr_block           = "10.0.3.0/24"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+module "vpc-2" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1b"]
+  private_subnets = ["10.0.3.0/24"]
+  public_subnets  = ["10.0.2.0/24"]
+
+
   tags = {
-    Name = "worker-vpc-zona_b"
+    Terraform = "true"
+    Environment = "dev"
   }
-
-}
-
-#Create IGW in us-east-1
-resource "aws_internet_gateway" "igw" {
-  provider = aws.region-master
-  vpc_id   = aws_vpc.vpc_master.id
-}
-
-#Get all available AZ's in VPC for master region
-data "aws_availability_zones" "azs" {
-  provider = aws.region-master
-  state    = "available"
-}
-
-
-#Create subnet # 1 in us-east-1
-resource "aws_subnet" "subnet_1" {
-  provider          = aws.region-master
-  availability_zone = element(data.aws_availability_zones.azs.names, 0)
-  vpc_id            = aws_vpc.vpc_master.id
-  cidr_block        = "10.0.1.0/24"
-}
-
-#Create subnet #2  in us-east-1
-resource "aws_subnet" "subnet_2" {
-  provider          = aws.region-master
-  vpc_id            = aws_vpc.vpc_master.id
-  availability_zone = element(data.aws_availability_zones.azs.names, 1)
-  cidr_block        = "10.0.2.0/24"
-}
-
-
-#Create subnet in us-west-2
-resource "aws_subnet" "subnet_1_oregon" {
-  provider   = aws.region-worker
-  vpc_id     = aws_vpc.vpc_master_oregon.id
-  cidr_block = "10.0.3.0/24"
 }
