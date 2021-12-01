f2bec53cf255b10f0adeb68a1c53eff16a231e27
KMerabEpam <95089207+KMerabEpam@users.noreply.github.com>
2021-12-01 13:09:04 +0400

    create

---
+++ Security_alb.tf
@@ -0,0 +1,94 @@
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+
+resource "aws_lb" "application-lb" {
+  provider           = aws
+  name               = "web-server1"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb-sg.id]
+  subnets            = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
+  tags = {
+    Name = "Webserver-1"
+  }
+}
+
+resource "aws_lb_target_group" "app-lb-tg" {
+  provider    = aws
+  name        = "app-lb-tg"
+  port        = 8080
+  target_type = "instance"
+  vpc_id      = aws_vpc.my-vpc1
+  protocol    = "HTTP"
+  health_check {
+    enabled  = true
+    interval = 10
+    path     = "/login"
+    port     = 8080
+    protocol = "HTTP"
+    matcher  = "200-299"
+  }
+  tags = {
+    Name = "webserver-target-group"
+  }
+}
+
+resource "aws_lb_listener" {
+  provider          = aws.region-master
+  load_balancer_arn = aws_lb.application-lb.arn
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate.jenkins-lb-https.arn
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app-lb-tg.arn
+  }
+}
+
+resource "aws_lb_listener" "jenkins-listener-http" {
+  provider          = aws.region-master
+  load_balancer_arn = aws_lb.application-lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_target_group_attachment" "jenkins-master-attach" {
+  provider         = aws.region-master
+  target_group_arn = aws_lb_target_group.app-lb-tg.arn
+  target_id        = aws_instance.jenkins-master.id
+  port             = 8080
+}
