09e2eefa2d83691cce7af5b8bcf34c5d48b3571b
Goga365 <goga@DESKTOP-5V434D3.localdomain>
2021-12-01 17:19:25 +0400

    FIX: NAT-Gateway Only One Availability Zone(Public-Subnet)

--- network-install.tf
+++ network-install.tf
@@ -23,6 +23,7 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.0.0/24", "10.0.2.0/24"]
 
+  enable_nat_gateway = true
   single_nat_gateway = true
   enable_vpn_gateway = true
 
