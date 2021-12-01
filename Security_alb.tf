f24fc22b5f7fbc282099c232795bcb0be69d4512
KMerabEpam <95089207+KMerabEpam@users.noreply.github.com>
2021-12-01 13:13:34 +0400

    update

--- Security_alb.tf
+++ Security_alb.tf
@@ -58,37 +58,4 @@ resource "aws_lb_target_group" "app-lb-tg" {
   }
 }
 
-resource "aws_lb_listener" {
-  provider          = aws.region-master
-  load_balancer_arn = aws_lb.application-lb.arn
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  port              = "443"
-  protocol          = "HTTPS"
-  certificate_arn   = aws_acm_certificate.jenkins-lb-https.arn
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app-lb-tg.arn
-  }
-}
 
-resource "aws_lb_listener" "jenkins-listener-http" {
-  provider          = aws.region-master
-  load_balancer_arn = aws_lb.application-lb.arn
-  port              = "80"
-  protocol          = "HTTP"
-  default_action {
-    type = "redirect"
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-}
-
-resource "aws_lb_target_group_attachment" "jenkins-master-attach" {
-  provider         = aws.region-master
-  target_group_arn = aws_lb_target_group.app-lb-tg.arn
-  target_id        = aws_instance.jenkins-master.id
-  port             = 8080
-}
