6bf8cf11857667b803673cd15cb92b0327a9b83f
KMerabEpam <95089207+KMerabEpam@users.noreply.github.com>
2021-12-01 13:34:59 +0400

    update

--- network-install.tf
+++ network-install.tf
@@ -19,3 +19,40 @@ resource "aws_internet_gateway" "igw" {
   provider = aws.region-master
   vpc_id   = aws_vpc.vpc_master.id
 }
+#Create route table in eu-west-1a
+resource "aws_route_table" "internet_route" {
+  provider = aws.region-master
+  vpc_id   = aws_vpc.vpc_master.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  lifecycle {
+    ignore_changes = all
+  }
+  tags = {
+    Name = "Webserver-1"
+  }
+ 
+  #Create subnet # 1 in eu-west-1a
+resource "aws_subnet" "subnet_1" {
+  provider          = aws.region-master
+  availability_zone = element(data.aws_availability_zones.azs.names, 0)
+  vpc_id            = aws_vpc.vpc_master.id
+  cidr_block        = "10.0.1.0/24"
+}
+
+#Create subnet #2  in eu-west-1b
+resource "aws_subnet" "subnet_2" {
+  provider          = aws.region-master
+  vpc_id            = aws_vpc.vpc_master.id
+  availability_zone = element(data.aws_availability_zones.azs.names, 1)
+  cidr_block        = "10.0.3.0/24"
+}
+
+#Overwrite default route table of VPC(Worker) with our route table entries
+resource "aws_main_route_table_association" "set-worker-default-rt-assoc" {
+  provider       = aws.region-master
+  vpc_id         = aws_vpc.vpc_master_oregon.id
+  route_table_id = aws_route_table.internet_route_oregon.id
+}
