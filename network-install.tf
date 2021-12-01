f168b1e75c01c90f1167d338a6eaede9d4c8bd6e
KMerabEpam <95089207+KMerabEpam@users.noreply.github.com>
2021-12-01 13:27:20 +0400

    update

--- network-install.tf
+++ network-install.tf
@@ -1,15 +1,12 @@
-module "vpc-1" {
+resource "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "my-vpc1"
+  name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a"]
-  private_subnets = ["10.0.1.0/24"]
-  public_subnets  = ["10.0.0.0/24"]
-
-  enable_nat_gateway = true
-
+  azs             = ["eu-west-1a", "eu-west-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.0.0/24", "10.0.2.0/24"]
 
   tags = {
     Terraform = "true"
@@ -17,19 +14,8 @@ module "vpc-1" {
   }
 }
 
-module "vpc-2" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "my-vpc2"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["eu-west-1b"]
-  private_subnets = ["10.0.3.0/24"]
-  public_subnets  = ["10.0.2.0/24"]
-
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
+#Create IGW in eu-west-1a
+resource "aws_internet_gateway" "igw" {
+  provider = aws.region-master
+  vpc_id   = aws_vpc.vpc_master.id
 }
