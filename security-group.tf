728731cd3da9fc4099e2ef7706ec0e0b714f1225
KMerabEpam <95089207+KMerabEpam@users.noreply.github.com>
2021-12-02 10:33:25 +0400

    create securite group

---
+++ security-group.tf
@@ -0,0 +1,24 @@
+module "vote_service_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "user-service"
+  description = "Security group"
+  vpc_id      = "module.vpc.vpc.id"
+
+  ingress_cidr_blocks      = ["10.10.0.0/16"]
+  ingress_rules            = ["https-443-tcp"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8090
+      protocol    = "tcp"
+      description = "User-service ports"
+      cidr_blocks = "10.10.0.0/16"
+    },
+    {
+      rule        = "postgresql-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+}
+
