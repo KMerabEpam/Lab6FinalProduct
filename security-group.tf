44ac774b253427c9f0387800121a99246e83ec33
KMerabEpam <95089207+KMerabEpam@users.noreply.github.com>
2021-12-02 11:15:33 +0400

    update

--- security-group.tf
+++ security-group.tf
@@ -1,7 +1,7 @@
-module "vote_service_sg" {
+module "web-service_sg" {
   source = "terraform-aws-modules/security-group/aws"
 
-  name        = "user-service"
+  name        = "web-service"
   description = "Security group"
   vpc_id      = "module.vpc.vpc.id"
 
@@ -22,3 +22,26 @@ module "vote_service_sg" {
   ]
 }
 
+resorce "web-service_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "web-service"
+  description = "Security group 80"
+  vpc_id      = "module.vpc.vpc.id"
+
+  ingress_cidr_blocks      = ["10.0.0.0/24"]
+  ingress_rules            = ["http-80-tcp"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8090
+      protocol    = "tcp"
+      description = "User-service ports"
+      cidr_blocks = "10.0.0.0/24"
+    },
+    {
+      rule        = "postgresql-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+}
